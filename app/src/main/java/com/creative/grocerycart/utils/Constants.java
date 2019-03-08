db0eb1fc94e018829055b16b2873fddf3d1ddd5f
Raj Sehmi <raj.sehmi1@yahoo.com>
2019-03-07 18:58:10 -0800

    Added Integer Constant that defines the time delayed to reset the exut flag for double tap operation

--- app/src/main/java/com/creative/grocerycart/utils/Constants.java
+++ app/src/main/java/com/creative/grocerycart/utils/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
     public static final int SUCCESS_CODE = 1;
     public static final int ERROR_CODE_SOME_ERROR_OCCURED = -2;
 
-    public static final int DELAY_TIME_IN_MILLIS = 500;
+    public static final int DELAY_TIME_IN_MILLIS = 300;
+    public static final int DELAY_TIME_EXIT_FLAG_RESET = 1000;
 }
